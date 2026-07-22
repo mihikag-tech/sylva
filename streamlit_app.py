013b4c610339b47ec208a9eaabbf0da30865476a
mihikag-tech <mihikag03@gmail.com>
2026-07-22 19:11:46 +0000

    App v1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pickle
 
-st.title("🎈 My new app")
+st.title("Welcome to Sylva!")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+loaded_model = pickle.load(open('xgb_model_pickle', 'rb'))
+
